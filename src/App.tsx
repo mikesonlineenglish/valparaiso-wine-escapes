ce69720337b693a1e7dcdd87f5de21586211f928
Asia <asia.n@eco-n-tech.com>
2026-02-25 08:34:04 -0300

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/valparaiso-wine-escapes/">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
